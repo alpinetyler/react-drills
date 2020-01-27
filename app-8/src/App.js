1be813cb194670b6314005f4d0e291b313677076
TylerT <alpinetyler@gmail.com>
2020-01-26 19:48:44 -0700

    re-wrote app-8 for practice

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,34 +1,35 @@
-import React, { Component } from "react";
-import "./App.css";
-import axios from 'axios'
+import React, {Component} from "react";
+import "./App.css"
+import axios from "axios";
 
-class App extends Component {
-  constructor(props){
-    super(props)
+export default class App extends Component{
+  constructor(){
+    super()
 
     this.state = {
       lukeSkywalker: ''
     }
   }
-
-  componentDidMount() {
-    axios.get("https://swapi.co/api/people/88").then(response => {
+  componentDidMount(){
+    axios.get("https://swapi.co/api/people/1").then(response => {
       this.setState({
         lukeSkywalker: response.data
-      })
-    })
+      });
+    });
   }
-  
-  render() {
-    return (
+
+  render(){
+    return(
       <div className="App">
-        <h1>Name: {this.state.lukeSkywalker.name}</h1>
-        <h1>Birth Year: {this.state.lukeSkywalker.birth_year}</h1>
-        <h1>Height: {this.state.lukeSkywalker.height}</h1>
-        <h1> Eye Color:{this.state.lukeSkywalker.eye_color}</h1>
+       <h3>Name: {this.state.lukeSkywalker.name}</h3>
+        <h3>Birth Year: {this.state.lukeSkywalker.birth_year}</h3>
+        <h3>Height: {this.state.lukeSkywalker.height}</h3>
+        <h3>Mass: {this.state.lukeSkywalker.mass}</h3>
+        <h3>Eye Color: {this.state.lukeSkywalker.eye_color}</h3>
+        <h3>Hair Color: {this.state.lukeSkywalker.hair_color}</h3>
+        <h3>Skin Color: {this.state.lukeSkywalker.skin_color}</h3>
+        <h3>Home Planet: {this.state.lukeSkywalker.home_planet}</h3>
       </div>
-    );
+    )
   }
 }
-
-export default App;
