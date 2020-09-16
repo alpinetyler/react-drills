920f52369698f94e857a722020f8666c88af168a
TylerT <alpinetyler@gmail.com>
2020-09-15 20:45:01 -0600

    react practice 9-15-2020

--- app-4/src/components/Login.js
+++ app-4/src/components/Login.js
@@ -1,14 +1,13 @@
-import React, {Component} from "react";
+import React, {Component} from "react"
 
 export default class Login extends Component{
     constructor(){
         super();
 
         this.state = {
-            username: '',
-            password: ''
-        }
-
+            username: "",
+            password: ""
+        };
     }
 
     handleChange = e => {
@@ -19,36 +18,30 @@ export default class Login extends Component{
     }
 
     handleLogin = () => {
-        alert(`Your username is '${this.state.username}' and your password is '${this.state.password}'`)
+        alert(`your username is '${this.state.username}' and your password is '${this.state.password}'`)
+        //reset state to blank
         this.setState({
-            username: '',
-            password: ''
-          })
+            username: "",
+            password:""
+        })
     }
-
-
-
     render(){
         return(
             <div>
-            <input
-            type="text"
-            name="username"
-            onChange={this.handleChange}
-            placeholder="Enter a Username" 
-            value={this.state.username}/>
-            <p>
-            <input
-            type="text"
-            name="password"
-            onChange={this.handleChange}
-            placeholder="Enter a Password"
-            value={this.state.password} /> 
-            </p>
-            <button onClick={this.handleLogin}>Login</button>
-        </div>
+                <input 
+                    name="username"
+                    onChange={this.handleChange}
+                    value={this.state.username}
+                    type="text"
+                />
+                <input 
+                    name="password"
+                    onChange={this.handleChange}
+                    value={this.state.password}
+                    type="text"
+                />
+                <button onClick={this.handleLogin}>Login</button>
+            </div>
         )
     }
-        
-        
 }
